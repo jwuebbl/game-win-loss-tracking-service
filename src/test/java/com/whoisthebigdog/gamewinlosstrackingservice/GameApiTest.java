c1f8919724b175014c3288ff425868fb4f1e8f88
Jeffrey Wuebbles <jwuebbl@siue.edu>
2023-11-21 12:48:56 -0600

    Organized the unit test files into three files

---
+++ src/test/java/com/whoisthebigdog/gamewinlosstrackingservice/GameApiTest.java
@@ -0,0 +1,67 @@
+package com.whoisthebigdog.gamewinlosstrackingservice;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import com.whoisthebigdog.gamewinlosstrackingservice.models.Game;
+import com.whoisthebigdog.gamewinlosstrackingservice.models.GameRecord;
+import com.whoisthebigdog.gamewinlosstrackingservice.models.Team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GameApiTest {
+    
+    
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void shouldReturnAGameWithAKnownId() {
+        ResponseEntity<String> response = restTemplate
+        .getForEntity("/games/5", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.gameId");
+        assertThat(id).isEqualTo(5);
+
+        String gameName = documentContext.read("$.gameName");
+        assertThat(gameName).isEqualTo("Golf");
+    }
+
+    @Test
+    void shouldNotReturnAGameWithAnUnknownId() {
+        ResponseEntity<String> response = restTemplate
+        .getForEntity("/games/1000", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isBlank();
+    }
+
+    @Test
+    void shouldCreateANewGame() {
+        Game newGame = new Game(null, "unitTestGame");
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/games", newGame, Void.class);
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        URI locationOfNewGame = createResponse.getHeaders().getLocation();
+        ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewGame, String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+        System.out.println(documentContext.jsonString());
+        Number gameId = documentContext.read("$.gameId");
+        assertThat(gameId).isNotNull();
+        String gameName = documentContext.read("$.gameName");
+        assertThat(newGame.gameName()).isEqualTo(gameName);
+    }
+}
